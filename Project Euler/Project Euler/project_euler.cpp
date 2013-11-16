e2e150a221452a021a751e35f90975a17908ac6b
Mindy Huang <mindyh@stanford.edu>
2013-11-16 10:31:31 -0800

    problem 4 and 15

--- Project Euler/Project Euler/project_euler.cpp
+++ Project Euler/Project Euler/project_euler.cpp
@@ -8,6 +8,7 @@
 using namespace std;
 
 // problem 3
+// O(n^2)
 long long largest_prime_factor(long long input)
 {
 	long long curr_factor = 2;
@@ -64,20 +65,22 @@ bool isPalindrome(int num)
 	string numStr  = itostr(num);
 	int strSize = numStr.size();
 	for(int i = 0; i < strSize/2; i++)
-		if(numStr[i] != numStr[strSize - i])
+		if(numStr[i] != numStr[strSize - i - 1])
 			return false;
 
 	return true;
 }
 
+
+// O(n^2)
 int largestPalindromeProduct()
 {
-	int x = 10;
-	int y = 10;
+	int x = 100;
 	int retval = 0;
-	while (x < 100)
+	while (x < 1000)
 	{
-		while(y < 100)
+		int y = 100;
+		while(y < 1000)
 		{
 			int z = x*y;
 			if (z > retval && isPalindrome(z))
@@ -92,6 +95,33 @@ int largestPalindromeProduct()
 }
 
 
+// Problem 15 
+/* given an n by n lattice, how many paths are possible 
+   from the top left corner to the bottom right?
+*/
+int latticeHelper(int x, int y, int n)
+{
+	int numPaths = 0;
+
+	if(x == n && y == n)
+	{
+		return 1;
+	}
+
+	if(x < n)
+		numPaths += latticeHelper(x+1, y, n);
+
+	if(y < n)
+		numPaths += latticeHelper(x, y+1, n);
+
+	return numPaths;
+}
+
+int LatticePaths(int n)
+{
+	return latticeHelper(0, 0, n);
+}
+
 
 int main()
 {
@@ -102,9 +132,16 @@ int main()
 	cin >> input;
 	cout << largest_prime_factor(input) << endl;*/
 	
+	// problem 4
+	// cout << "largest palindrome " << largestPalindromeProduct() << endl;
 
-	cout << "largest palindrome " << largestPalindromeProduct() << endl;
+	// Problem 5 
+	cout << "lattice paths: " << LatticePaths(20) << endl;
+	string c = "";
+	c += "a";
+	cout << c
 
+	/* prevent window from closing */
 	cout << "done" << endl; 
 	int c; 
 	cin >> c;
